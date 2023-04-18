eeb892b81a989c57730beb1797140441c9fad12e
Anton Nikitin <antonnikitin@live.co.uk>
2023-04-18 23:22:25 +0100

    update slobs code

--- Project_Buran_Controller/SLOBSLocal.cs
+++ Project_Buran_Controller/SLOBSLocal.cs
@@ -15,44 +15,59 @@ namespace Buran_Controller
 {
     public class SLOBSLocal
     {
-        private SlobsPipeClient pipeClient;
-        private string apiKey;
+        private static readonly SlobsPipeClient pipeClient = new SlobsPipeClient("slobs");
+        private static readonly string apiKey = Environment.GetEnvironmentVariable("SLOBSAPI");
+        public static Dictionary<String, String> SceneValuePairs { get; private set; }
 
-        public SLOBSLocal()
+        static SLOBSLocal()
         {
-            this.pipeClient = new SlobsPipeClient("slobs");
-            this.apiKey = "";
-        }
+            SceneValuePairs = new Dictionary<string, string>();
 
-        public void authRequest()
-        {
-            issueRequest(SlobsRequestBuilder.NewRequest().SetMethod("auth").SetResource("TcpServerService").AddArgs(new string [] { apiKey }).BuildRequest());
+            AuthRequest();
+            GetSceneNamesIDs();
         }
 
         /// <summary>
-        /// Returns a dictionary with scene names and IDs from within SLOBS
+        /// Issues an authentication request to the client so we can control it.
         /// </summary>
-        public Dictionary<String, String> getSceneNamesIDs()
+        private static void AuthRequest()
         {
-            List<SlobsResult> responses = issueRequest(SlobsRequestBuilder.NewRequest().SetMethod("getScenes").SetResource("ScenesService").BuildRequest()).Result.ToList();
+            IssueRequest(SlobsRequestBuilder.NewRequest().SetMethod("auth").SetResource("TcpServerService").AddArgs(new string [] { apiKey }).BuildRequest());
+        }
+
+        /// <summary>
+        /// Issues a request to the SLOBS service and forms a dictionary of scene names and IDs to be used later.
+        /// </summary>
+        private static void GetSceneNamesIDs()
+        {
+            List<SlobsResult> responses = IssueRequest(SlobsRequestBuilder.NewRequest().SetMethod("getScenes").SetResource("ScenesService").BuildRequest()).Result.ToList();
             Dictionary<String, String> scenes = new Dictionary<String, String>();
 
             foreach(SlobsResult sr in responses)
             {
-                scenes.Add(sr.Id, sr.Name);
+                scenes.Add(sr.Name, sr.Id);
             }
 
-            return scenes;
+            SceneValuePairs = scenes;
         }
-
-        public void switchToScene(string scene)
+        
+        /// <summary>
+        /// Issues a request to the SLOBS service to switch to a particular scene.
+        /// </summary>
+        /// <param name="scene">The scene ID to switch to.</param>
+        public static void SwitchToScene(string scene)
         {
-            issueRequest(SlobsRequestBuilder.NewRequest().SetMethod("makeSceneActive").SetResource("ScenesService").AddArgs(new string[] { scene }).BuildRequest());
+            IssueRequest(SlobsRequestBuilder.NewRequest().SetMethod("makeSceneActive").SetResource("ScenesService").AddArgs(new string[] { SceneValuePairs[scene] }).BuildRequest());
         }
-
-        private SlobsRpcResponse issueRequest(ISlobsRequest request)
+        
+        /// <summary>
+        /// Issues a request to the SLOBS service and returns the response
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>The response of the request</returns>
+        private static SlobsRpcResponse IssueRequest(ISlobsRequest request)
         {
-            return this.pipeClient.ExecuteRequest(request);
+            return pipeClient.ExecuteRequest(request);
         }
     }
 }
